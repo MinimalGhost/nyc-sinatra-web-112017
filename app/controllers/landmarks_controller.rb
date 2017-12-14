258b376ef6212f149ea66729a47be7e033c723cf
github username <github email address>
2017-12-14 13:24:03 -0500

    migrations, models, routes for landmarks

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,38 @@
 class LandmarksController < ApplicationController
 
+  #INDEX
+  get '/landmarks/' do
+    erb :'/landmarks/index'
+  end
+
+  #CREATE
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    erb :'/landmarks'
+  end
+
+  #SHOW
+  get '/landmarks/:id' do
+    erb :'/landmarks/show'
+  end
+
+  #EDIT
+  get '/landmarks/:id/edit' do
+
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+
+    erb :'/landmarks/show'
+  end
+
+  #DELETE
+  delete '/landmarks/:id/delete' do
+
+    redirect to '/landmarks'
+  end
 end
